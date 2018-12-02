5aadc6f2cb2afb085d9bc50dc64cf1fdd576d5fa
Ray Villalobos <ray@planetoftheweb.com>
2018-12-01 19:27:20 -0500

    cleanups

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Output,
+  EventEmitter
+} from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 export class SearchComponent implements OnInit {
-
   @Output() queryEvt = new EventEmitter<string>();
 
   handleQuery(query: string) {
