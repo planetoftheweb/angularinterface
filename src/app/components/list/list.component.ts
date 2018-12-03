824f9d4622923d4ce68f4b582c0e29dabd32ea13
Ray Villalobos <ray@planetoftheweb.com>
2018-12-02 17:15:37 -0800

    04_06e

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -12,8 +12,17 @@ import {
 export class ListComponent {
   @Input() aptList;
   @Output() deleteEvt = new EventEmitter();
+  @Output() updateEvt = new EventEmitter();
 
   handleDelete(theApt: object) {
     this.deleteEvt.emit(theApt);
+  }
+
+  handleUpdate(theApt: object, labelName: string, newValue: string) {
+    this.updateEvt.emit({
+      theApt: theApt,
+      labelName: labelName,
+      newValue: newValue
+    });
   }
 }
